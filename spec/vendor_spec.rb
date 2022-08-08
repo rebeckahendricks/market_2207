fdc7189e35fca57a4427c93da44c56a134555d6f
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-08 09:27:39 -0600

    Test: Add tests for stock, check_stock, and inventory methods

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -19,4 +19,37 @@ describe Vendor do
       expect(@vendor.inventory).to eq({})
     end
   end
+
+  describe 'checking stock' do
+    before do
+      @vendor = Vendor.new("Rocky Mountain Fresh")
+      @item1 = Item.new({name: 'Peach', price: "$0.75"})
+      @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    end
+
+    xit 'can check the stock of a certain item' do
+      expect(@vendor.check_stock(@item1)).to eq(0)
+    end
+
+    xit 'can stock an item and check its inventory' do
+      @vendor.stock(@item1, 30)
+
+      expect(@vendor.inventory).to eq({@item1 => 30})
+
+      @vendor.stock(@item1, 25)
+      @vendor.stock(@item2, 12)
+
+      expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
+    end
+
+    xit 'can check the stock of a certain item' do
+      @vendor.stock(@item1, 30)
+
+      expect(@vendor.check_stock(@item1)).to eq(30)
+
+      @vendor.stock(@item1, 25)
+
+      expect(@vendor.check_stock(@item1)).to eq(55)
+    end
+  end
 end
