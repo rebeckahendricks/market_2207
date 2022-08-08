8744a5d5d49245893be074de3c339c6eacf561ef
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-08 09:49:41 -0600

    Test: Add tests in market_spec and vendor_spec for add_vendor, vendor_names, vendors_that_sell, and potential_revenue

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -51,5 +51,31 @@ describe Vendor do
 
       expect(@vendor.check_stock(@item1)).to eq(55)
     end
+
+    describe 'potentail income' do
+      before do
+        @vendor1 = Vendor.new("Rocky Mountain Fresh")
+        @item1 = Item.new({name: 'Peach', price: "$0.75"})
+        @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+        @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+        @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+        @vendor1.stock(@item1, 35)
+        @vendor1.stock(@item2, 7)
+
+        @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+        @vendor2.stock(@item4, 50)
+        @vendor2.stock(@item3, 25)
+
+        @vendor3 = Vendor.new("Palisade Peach Shack")
+        @vendor3.stock(@item1, 65)
+      end
+
+      xit 'can calculate its potential revenue' do
+        expect(@vendor1.potential_revenue).to eq(29.75)
+        expect(@vendor2.potential_revenue).to eq(345.00)
+        expect(@vendor3.potential_revenue).to eq(48.75)
+      end
+    end
+
   end
 end
