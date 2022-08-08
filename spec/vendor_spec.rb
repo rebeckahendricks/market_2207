5ae97db2e08a3311f6c4003bb02e7e219e124668
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-08 09:11:40 -0600

    Feat: Create Vendor class and attributes

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -4,20 +4,18 @@ require './lib/vendor'
 describe Vendor do
   describe 'initialize' do
     before do
-      # @item1 = Item.new({name: 'Peach', price: "$0.75"})
-      # @item2 = Item.new({name: 'Tomato', price: '$0.50'})
       @vendor = Vendor.new("Rocky Mountain Fresh")
     end
 
-    xit 'exists' do
+    it 'exists' do
       expect(@vendor).to be_instance_of(Vendor)
     end
 
-    xit 'has a name' do
+    it 'has a name' do
       expect(@vendor.name).to eq("Rocky Mountain Fresh")
     end
 
-    xit 'has no inventory by default' do
+    it 'has no inventory by default' do
       expect(@vendor.inventory).to eq({})
     end
   end
