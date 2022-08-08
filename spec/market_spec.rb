adaf1a60afe87dab4f276129e689ac5f233ac3fc
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-08 10:20:21 -0600

    Test: Add tests for total_inventory, overstocked_items, and sorted_item_list methods

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -66,4 +66,53 @@ describe Market do
       expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
     end
   end
+
+  describe 'items sold at the market' do
+    before do
+      @market = Market.new("South Pearl Street Farmers Market")
+      @item1 = Item.new({name: "Peach", price: "$0.75"})
+      @item2 = Item.new({name: "Tomato", price: "$0.50"})
+      @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      @vendor1 = Vendor.new("Rocky Mountain Fresh")
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+
+      @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+
+      @vendor3 = Vendor.new("Palisade Peach Shack")
+      @vendor3.stock(@item1, 65)
+      @vendor3.stock(@item3, 10)
+
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+    end
+
+    xit 'can display its total inventory' do
+      expected = {
+        @item1 => { quantity: 100,
+                    vendors: [@vendor1, @vendor3]},
+        @item2 => { quantity: 7,
+                    vendors: [@vendor1]},
+        @item3 => { quantity: 35,
+                    vendors: [@vendor2, @vendor3]},
+        @item4 => { quantity: 50,
+                    vendors: [@vendor2]}
+      }
+
+      expect(@market.total_inventory).to eq(expected)
+    end
+
+    xit 'can identify overstocked items' do
+      expect(@market.overstocked_items).to eq([@item1])
+    end
+
+    xit 'can list the names of all items the vendors have in stock, sorted alphabetically' do
+      expect(@market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
+    end
+  end
 end
