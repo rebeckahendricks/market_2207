3a63160fcad3d682224d16ab622b8bce3e9526cd
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-08 09:38:01 -0600

    Test: Create Market class initialize tests

---
+++ spec/market_spec.rb
@@ -0,0 +1,23 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+describe Market do
+  describe 'initialize' do
+    before do
+      @market = Market.new("South Pearl Street Farmers Market")
+    end
+
+    xit 'exists' do
+      expect(@market).to be_instance_of(Market)
+    end
+
+    xit 'has a name' do
+      expect(@market.name).to eq("South Pearl Street Farmers Market")
+    end
+
+    xit 'has no vendors by default' do
+      expect(@market.vendors).to eq([])
+    end
+  end
+end
